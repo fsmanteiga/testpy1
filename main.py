a9708212082943dc0a6164555de682bb1550c1bc
fsmanteiga <fsmanteiga@none.no>
2023-02-11 19:40:30 +0100

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
 
 # Just a quick test
-print("[fsmanteiga python test]")
+print("[fsmanteiga python tests]")
+
+def printDict(dict):
+    for e in dict:
+        print(str(e) + " " + str(dict[e]) )
+
+# Declare list
+myList=[]
+
+for i in range(0,20):
+    print(str(i)+" Added")
+    myList.append(i)
+
+# Initialize index i
+i = 0
+for element in myList:
+    print("element "+str(i)+" is : "+str(element))
+    i = i + 1
+
+print("**********************")
+
+dic = {}
+for i in range(7, 0, -1):
+    dic[i] = i*i
+
+printDict(dic)
 
 
 
